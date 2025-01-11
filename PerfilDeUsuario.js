9c1ad5e65e0cbebda2983a582c4ee94795d8336b
millug <milangel.lugo2312@gmail.com>
2025-01-11 22:30:11 +0100

    Perfil de usuario

---
+++ PerfilDeUsuario.js
@@ -0,0 +1,22 @@
+// Variables para un perfil de usuario.
+const nombreCompleto = "Milangel Carolina Lugo Moreno";
+let edad = "20 años";
+let peso = "63 Kg";
+let altura = "1,70 m";
+
+// Imprimir el perfil inicial.
+console.log("Perfil del usuario:");
+console.log("Nombre:", nombreCompleto);
+console.log("Edad:", edad);
+console.log("Peso:", peso);
+console.log("Altura:", altura);
+
+// Cambiar una de las variables.
+edad = "21 años"; // Cambiar la edad del usuario.
+
+// Imprimir el perfil actualizado
+console.log("\nPerfil actualizado del usuario:");
+console.log("Nombre:", nombreCompleto);
+console.log("Edad:", edad); // Ahora muestra la edad actualizada.
+console.log("Peso:", peso);
+console.log("Altura:", altura);
